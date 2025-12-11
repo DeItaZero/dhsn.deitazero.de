905bd4efad799368e0fa5bd05b069e445ce5cec5
Saftibims <kruggelnoah@gmail.com>
2025-12-11 20:01:26 +0100

    imported nestjs config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,16 @@ import { GroupsModule } from './groups/groups.module';
 import { TimetableModule } from './timetable/timetable.module';
 import { Module } from '@nestjs/common';
 import { ModulesModule } from './modules/modules.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GroupsModule, TimetableModule, ModulesModule],
+  imports: [
+    GroupsModule,
+    TimetableModule,
+    ModulesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
